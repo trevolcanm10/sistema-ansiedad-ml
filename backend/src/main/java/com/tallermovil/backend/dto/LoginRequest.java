5dec5628f8bb9c08ec2786185c2997aeb0e97a3c
trevolcanm10 <denilson.morales@unmsm.edu.pe>
2026-05-18 00:44:45 -0500

    Preparación de la seguridad del login

---
+++ backend/src/main/java/com/tallermovil/backend/dto/LoginRequest.java
@@ -0,0 +1,12 @@
+package com.tallermovil.backend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+    
+    private String email;
+    private String password;
+}
