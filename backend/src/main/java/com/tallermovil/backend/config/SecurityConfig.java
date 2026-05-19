601a60e7bf1058f31edfba886db55a586c2d6bfe
trevolcanm10 <denilson.morales@unmsm.edu.pe>
2026-05-19 00:50:00 -0500

    Modificación del archivo de seguridad por que protegia todo los endpoints

--- backend/src/main/java/com/tallermovil/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/tallermovil/backend/config/SecurityConfig.java
@@ -2,12 +2,38 @@ package com.tallermovil.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
 @Configuration
 public class SecurityConfig {
+
+    // Bean para encriptar passwords
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
-    
+
+    // Configuración de seguridad
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http)
+            throws Exception {
+
+        http
+            // Desactivar CSRF
+            .csrf(csrf -> csrf.disable())
+
+            // Configurar permisos
+            .authorizeHttpRequests(auth -> auth
+
+                // Permitir auth sin token
+                .requestMatchers("/auth/**").permitAll()
+
+                // Cualquier otra ruta requiere login
+                .anyRequest().authenticated()
+            );
+
+        return http.build();
+    }
 }
